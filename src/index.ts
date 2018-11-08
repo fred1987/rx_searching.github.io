7f6b0f47b2daa66665e1d02d82d4fc1b45414f96
Fred <hi@ifred.ru>
2018-11-08 08:17:44 +0300

    working

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-import { from, fromEvent, interval } from 'rxjs'
-import { debounce, map, pluck, switchMap } from 'rxjs/operators'
+import { from, fromEvent, Observable } from 'rxjs'
+import { debounceTime, distinctUntilChanged, map, pluck, switchMap } from 'rxjs/operators'
 
-const input: HTMLInputElement = document.querySelector('input')
-const res: HTMLDivElement = document.querySelector('#result')
+const inputEl: HTMLInputElement = document.querySelector('input') as HTMLInputElement
+const resEl: HTMLDivElement = document.querySelector('#result') as HTMLDivElement
 
 type res = { incomplete_results: boolean, items: Array<any>, total_count: number }
 
-function request$(str): Promise<res> {
+function request$(str: string): Promise<res> {
     return fetch(`https://api.github.com/search/repositories?q=${str}&sort=stars&order=desc`)
         .then((res: Response) => res.json())
 }
 
-fromEvent(input, 'input').pipe(
-    pluck('target', 'value'),
-    debounce(() => interval(1000)),
-    switchMap(x => from(request$(x))),
+const stream$: Observable<string> = fromEvent(inputEl, 'input').pipe(
+    pluck<Event, string>('target', 'value'),
+    debounceTime(1000),
+    distinctUntilChanged(),
+    switchMap<string, res>((x: string) => from(request$(x))),
     map((x: res) => x.items.map(x => `<p>${x.html_url}</p>`).join(''))
-).subscribe(
-    (data: string) => res.innerHTML = data
+)
+
+stream$.subscribe(
+    (data: string) => resEl.innerHTML = data
 )
