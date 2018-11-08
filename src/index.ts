46a1610043c00d0e8b3815b18bc7a8b799f068b2
Fred <hi@ifred.ru>
2018-11-08 08:51:41 +0300

    working

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ function request$(str: string): Promise<res> {
 
 const stream$: Observable<string> = fromEvent(inputEl, 'input').pipe(
     pluck<Event, string>('target', 'value'),
-    debounceTime(700),
+    debounceTime(500),
     distinctUntilChanged(),
     switchMap<string, res | string>((x: string) => (x.length > 0) ? from(request$(x)) : of('')),
     map((x: res | string) => (typeof x === 'string') ? '' : x.items.map(x => `<p>${x.html_url}</p>`).join(''))
