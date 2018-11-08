dac08069b74202c458af7859369304d4d027435b
Fred <hi@ifred.ru>
2018-11-08 08:43:17 +0300

    working

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const stream$: Observable<string> = fromEvent(inputEl, 'input').pipe(
     pluck<Event, string>('target', 'value'),
     debounceTime(700),
     distinctUntilChanged(),
-    switchMap<string, res>((x: string) => (x.length > 0) ? from(request$(x)) : of('')),
+    switchMap<string, res | string>((x: string) => (x.length > 0) ? from(request$(x)) : of('')),
     map((x: res | string) => (typeof x === 'string') ? '' : x.items.map(x => `<p>${x.html_url}</p>`).join(''))
 )
 
